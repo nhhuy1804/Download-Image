3778358ad5470d7296a56829e767afd4c1736521
MrDummy <mrdummy@MrDummys-Mac.local>
2017-07-05 09:54:07 +0700

    Download Images

--- Download/ViewController.swift
+++ Download/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgDownload: UIImageView!
+    @IBOutlet weak var txtURL: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnDownload(_ sender: Any) {
+        if txtURL.text == "" {
+            let alert = UIAlertController(title: "Error", message: "Please input URL", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            let url = txtURL.text
+            downloadImageWithURL(url!)
+        }
+    }
 
+    @IBAction func btnClear(_ sender: Any) {
+        txtURL.text = ""
+        imgDownload.image = nil
+    }
+    
+    func downloadImageWithURL(_ url: String) {
+        let imgURL = URL(string: url)
+        (URLSession(configuration: .default)).dataTask(with: imgURL!, completionHandler: {(dataImage, response, error) in
+            if let data = dataImage {
+                DispatchQueue.main.async() { () -> Void in
+                    self.imgDownload.image = UIImage(data: data)
+                }
+            }
+        }).resume()
+    }
 }
 
